45e2a40c787da520db76ccfa426d5d9c8b3551b2
Pierre Goiffon <pierre.goiffon@combodo.com>
2020-02-14 17:25:38 +0100

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# itop-sticky-caselog
+
+
+## Description
+
+[iTop](https://gihub.com/Combodo/iTop) module. Allows to make some caselog extracts sticky (always displayed), in order to inmprove
+ caselog readability.  
+
+
+## TODO
+
+- [ ] Action icon
+- [ ] selection save
+- [ ] display of saved extract
---
+++ module.itop-sticky-caselog.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ *
+ * @copyright   Copyright (C) 2010-2019 Combodo SARL
+ * @license     https://www.combodo.com/documentation/combodo-software-license.html
+ *
+ */
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'itop-sticky-caselog/0.0.1',
+	array(
+		// Identification
+		//
+		'label' => 'Allow to make caselog extracts sticky',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'main.itop-sticky-caselog.php',
+		),
+		'webservice' => array(
+
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any
+
+		// Default settings
+		//
+		'settings' => array(
+			'enabled' => true,
+		),
+	)
+);
