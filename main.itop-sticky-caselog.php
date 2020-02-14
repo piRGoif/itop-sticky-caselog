4279d6952cbc6d16a68a81f4cd086dbf8f6f2c1d
Pierre Goiffon <pierre.goiffon@combodo.com>
2020-02-14 17:53:57 +0100

    :construction: Add JS for Ticket

--- main.itop-sticky-caselog.php
+++ main.itop-sticky-caselog.php
@@ -1,2 +1,15 @@
 <?php
-//TODO
+
+class StickyCaseLogUiExtension extends AbstractApplicationUIExtension
+{
+	public function OnDisplayProperties($oObject, WebPage $oPage, $bEditMode = false)
+	{
+		if (!($oObject instanceof Ticket)) //FIXME should detect caselog automatically
+		{
+			return;
+		}
+
+		$sModuleJsUrl = utils::GetAbsoluteUrlModulesRoot().basename(__DIR__).'/sticky-caselog.js';
+		$oPage->add_linked_script($sModuleJsUrl);
+	}
+}
